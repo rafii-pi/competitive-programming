8da7fa666ec5035cffbb0a804c80c0cf6bc38804
rafii-pi <mrafifatihulihsan@gmail.com>
2025-08-21 20:58:50 +0700

    cihuyy

---
+++ compfest17-cpc/a_plus_b.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int long long
+#define rafiieeyy ios_base::sync_with_stdio(false); cin.tie(NULL)
+using namespace std;
+
+int32_t main(){
+    rafiieeyy;
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+}
